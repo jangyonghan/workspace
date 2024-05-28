3630dbb4e4ff70df0c9c446d9fd500f7a1672dd2
jangyonghan <wkddydgks1@gmail.com>
2024-05-28 13:46:14 +0900

    Add get_Percent funtion

--- calculator.py
+++ calculator.py
@@ -4,12 +4,18 @@ def subtract(a, b):
   return a-b
 def multiply(a, b):
    return a*b
-def divide_free(a, b):
-    return a/b
+
 def getMedian(a, b):
     return (a+b)/2
+
 def get_Abs(num):
    if mum>=0:
       return num
    else:
       return -num
+   
+def getRemainder(a, b):
+    return a//b
+
+def get_Percent(a, b):
+   return (a/b) *100
